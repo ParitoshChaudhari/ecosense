5ff992b0f47fd7e8231633e406611acd060f1ce0
tnem <tanmay.e.mahale@gmail.com>
2023-09-21 21:05:37 +0530

    fixed-footer-error

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -1,56 +1,67 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <>
-            <footer className="footer-section">
-                <div className="container bg-zinc-950">
-                    <div className="footer-content pt-3 pb-2">
-                        <div className="row">
-                            <div className="col-xl-4 col-lg-4 mb-50">
-                                <div className="footer-widget">
-                                    <div className="footer-logo">
-                                        <img src="logo.png" className="img-fluid ms-6" alt="logo" />
-                                    </div>
-                                    <div className="footer-text">
-                                        <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
-                                            elit,Lorem ipsum dolor sit amet.</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+  return (
+    <>
+      <footer className="footer-section">
+        <div className="container bg-zinc-950">
+          <div className="footer-content pt-3 pb-2">
+            <div className="row">
+              <div className="col-xl-4 col-lg-4 mb-50">
+                <div className="footer-widget">
+                  <div className="footer-logo">
+                    <img src="logo.png" className="img-fluid ms-6" alt="logo" />
+                  </div>
+                  <div className="footer-text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
+                      sed do eiusmod tempor incididuntut consec tetur
+                      adipisicing elit,Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
                 </div>
-                <div className="copyright-area">
-                    <div className="container">
-                        <div className="row">
-                        <div classNameName="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-                                <div className="footer-menu me-5">
-                                    <ul>
-                                        <li><a href="#">Home</a></li>
-                                        <li><a href="#">FAQ's</a></li>
-                                        <li><a href="#">Privacy</a></li>
-                                        <li><a href="#">Policy</a></li>
-                                        <li><a href="#">Contact</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
-                                <div className="copyright-text">
-                                    <p>Copyright &copy; 2023, All Right Reserved Team invisible</p>
-                                </div>
-                            </div>
-                           
-                        </div>
-                    </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="copyright-area">
+          <div className="container">
+            <div className="row">
+              <div classNameName="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                <div className="footer-menu me-5">
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">FAQ's</a>
+                    </li>
+                    <li>
+                      <a href="#">Privacy</a>
+                    </li>
+                    <li>
+                      <a href="#">Policy</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact</a>
+                    </li>
+                  </ul>
                 </div>
-            </footer>
+              </div>
+              <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+                <div className="copyright-text">
+                  <p>
+                    Copyright &copy; 2023, All Right Reserved Team invisible
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Footer
-
+export default Footer;
