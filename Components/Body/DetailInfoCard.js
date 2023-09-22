43392ced073653de5db5646dacec7811ef7a68cc
Paritosh Chaudhari <chaudhariparitosh9571@gmail.com>
2023-09-22 20:14:22 +0530

    Some optimization of mobile and 2 tables added

--- Components/Body/DetailInfoCard.js
+++ Components/Body/DetailInfoCard.js
@@ -17,11 +17,70 @@ const DetailsInfoCard = () => {
 
         <div className="col-span-2 lg:col-span-1 bg-gray-300 p-4 info-container">
           <h2 className="text-xl font-bold">AQI Readings</h2>
-          {/* <DataTable/> */}
+          <div className="table-container">
+            <div className="table-con">
+              <table className="custom-cell" >
+                <thead className="custom-cell">
+                  <tr className="custom-cell">
+                    <th >Parameter</th>
+                    <th > Value</th>
+                  </tr>
+                </thead>
+                <tbody className="custom-cell">
+                  <tr className="custom-cell">
+                    <td>AQI Level (PPM)</td>
+                    <td>44</td>
+                  </tr>
+                  <tr className="custom-cell">
+                    <td>Temperature (°C)</td>
+                    <td>35</td>
+                  </tr>
+                  <tr className="custom-cell">
+                    <td>Humidity (%)</td>
+                    <td>25</td>
+                  </tr>
+                  <tr className="custom-cell">
+                    <td>CO ()</td>
+                    <td>45</td>
+                  </tr>
+                  <tr className="custom-cell">
+                    <td>PM<sub>2.5</sub> (PPM)</td>
+                    <td>45</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
 
         <div className="col-span-2 lg:col-span-1 bg-gray-300 p-4 info-container">
           <h2 className="text-xl font-bold">WQI Readings</h2>
+          <div className="table-container">
+            <div className="table-con">
+              <table className="custom-cell" >
+                <thead className="custom-cell">
+                  <tr className="custom-cell">
+                    <th >Parameter</th>
+                    <th > Value</th>
+                  </tr>
+                </thead>
+                <tbody className="custom-cell">
+                  <tr className="custom-cell">
+                    <td>TDS Level (PPM)</td>
+                    <td>?</td>
+                  </tr>
+                  <tr className="custom-cell">
+                    <td>PH Level (pH)</td>
+                    <td>?</td>
+                  </tr>
+                  <tr className="custom-cell">
+                    <td>Purity (%)</td>
+                    <td>?</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
