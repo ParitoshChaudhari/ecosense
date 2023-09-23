056f44596f3ebb516f88811ab267afe8d1b5fbd1
tnem <tanmay.e.mahale@gmail.com>
2023-09-23 09:27:10 +0530

    Data usage covered

--- Components/Map/Map.js
+++ Components/Map/Map.js
@@ -13,55 +13,40 @@ const Map = () => {
   const database = FirebaseConfig();
   const dbref = ref(database);
 
+  const getData = () => {
+    const newMarkers = [];
+    get(child(dbref, "aqi/update")).then((snapshot) => {
+      snapshot.forEach((element) => {
+        const marker = {
+          lat: element.val().lat,
+          long: element.val().long,
+          co: element.val().co,
+          temp: element.val().temperature,
+          humid: element.val().humidity,
+          aqi: element.val().aqi,
+          id: element.val().id,
+          pm2: element.val().pm2,
+          type: "aqi",
+        };
+        newMarkers.push(marker);
+      });
+      setMarkers(newMarkers);
+      console.log(markers);
+    });
+  };
   // console.log("Getting Data");
   useEffect(() => {
     // Get the markers from the Firebase Database
-    get(child(dbref, "aqi/update")).then((snapshot) => {
-      const newMarkers = [];
-      snapshot.forEach((element) => {
-        const marker = {
-          lat: element.val().lat,
-          long: element.val().long,
-          co: element.val().co,
-          temp: element.val().temperature,
-          humid: element.val().humidity,
-          aqi: element.val().aqi,
-          id: element.val().id,
-        };
-        newMarkers.push(marker);
-      });
-      setMarkers(newMarkers);
-      // console.log(markers);
-    });
+    getData();
   }, []);
   // console.log("Data Retrived");
   // console.log(markers);
-  const updateData = () => {
-    // Get the markers from the Firebase Database
-    get(child(dbref, "aqi/update")).then((snapshot) => {
-      const newMarkers = [];
-      snapshot.forEach((element) => {
-        const marker = {
-          lat: element.val().lat,
-          long: element.val().long,
-          co: element.val().co,
-          temp: element.val().temperature,
-          humid: element.val().humidity,
-          aqi: element.val().aqi,
-          id: element.val().id,
-        };
-        newMarkers.push(marker);
-      });
-      setMarkers(newMarkers);
-      // console.log(markers);
-    });
-  };
-  setInterval(updateData, 2000);
+  setInterval(getData, 60 * 1000);
   return (
     <>
       <MapContainer
         center={[21.361673, 74.878475]}
-        zoom={16}
+        zoom={15}
         scrollWheelZoom={false}
       >
         <TileLayer
@@ -85,11 +70,17 @@ const Map = () => {
             {/* {console.log({ i })} */}
             <Popup>
               <>
-                <h2>AQI: {marker.aqi}</h2>
-                <hr />
-                <h3>CO Level: {marker.co}ppm</h3>
-                <h3>Temperature: {marker.temp}°C</h3>
-                <h3>Humidity: {marker.humid}%</h3>
+                <div className="aqiOpt">
+                  <h2>AQI: {marker.aqi} ppm</h2>
+                  <hr />
+                  <h3>CO Level: {marker.co} ppm</h3>
+                  <hr />
+                  <h3>PM2.5: {marker.pm2} ppm</h3>
+                  <hr />
+                  <h3>Temperaure: {marker.temp}°C</h3>
+                  <hr />
+                  <h3>Humidity: {marker.humid}%</h3>
+                </div>
               </>
             </Popup>
           </Marker>
